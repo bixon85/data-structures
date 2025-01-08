bc94f979098c3c4798333c370eb6e05218ad968e
bixon85 <155192381+bixon85@users.noreply.github.com>
2025-01-08 23:04:47 +0530

    using the stl stack is buid

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 int main(){
-    
+    //using the stack as the stl
+    stack<int>s;//the empty stack
+    s.push(1);//pushing the element in the stack
+    cout<<s.top()<<endl;//this will give the top element of the stack   
+    cout<<s.size()<<endl;//this will give the size of the stack
+    cout<<s.empty()<<endl;//this will check if the stack is empty or not
+    s.push(2);
+    //basically using the swap we can swap the two stacks
     return 0;
 }
