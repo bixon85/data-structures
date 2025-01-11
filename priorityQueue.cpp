b80228cf32b533a5ed9fa87abb4f84db1664f7d9
bixon85 <155192381+bixon85@users.noreply.github.com>
2025-01-11 18:37:08 +0530

    cretion of map and the priority queue

---
+++ priorityQueue.cpp
@@ -0,0 +1,12 @@
+// the first element of the priority queue is alaway the greatest
+// default priority queue is alawys a max heap 
+// on fetching the element that comes out is the alaways the greatest of them
+//
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+    priority_queue<int> p;
+    
+    return 0;
+}
