ce67462b8e1c17142e3b708266d6e4e0ab467266
bixon85 <155192381+bixon85@users.noreply.github.com>
2025-01-11 19:05:55 +0530

    this map

--- map.cpp
+++ map.cpp
@@ -1,8 +1,19 @@
 # include<iostream>
+#include<string>
 #include<map>
 using namespace std;
 int main(){
-    map<int,int>m;
+    //all keys are unique and one key point to one element 
+    // all are int he orderd way
+    map<int,string>m;
+    m.insert({5,"love"});
+    m.insert({1,"i"});
+    m.insert({2,"hello"});
+    for (auto i :m)
+    {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
     
+
     return 0;
 }
